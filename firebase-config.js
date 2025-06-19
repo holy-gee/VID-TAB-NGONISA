a238b48086b0d926ed8788bb1c7f52fe4a7ecc02
holy-gee <takundangonisa7@gmail.com>
2025-06-19 19:04:56 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Your Firebase config
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
