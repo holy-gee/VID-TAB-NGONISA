c3d5f17b27a79400908d2d88e77bfd80fe25363c
holy-gee <takundangonisa7@gmail.com>
2025-06-19 20:46:04 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,20 @@
-// Your Firebase setup
+// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Replace with your actual Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBPdGAZT_U8xNBsU-S4NnC7WUQI8zM1LWI",
   authDomain: "vidfind-77a6a.firebaseapp.com",
   projectId: "vidfind-77a6a",
   storageBucket: "vidfind-77a6a.appspot.com",
   messagingSenderId: "813301438270",
-  appId: "1:813301438270:web:2ebe4dec657167c5403e6f"
+  appId: "1:813301438270:web:2ebe4dec657167c5403e6f",
+  measurementId: "G-N4NTHY2230"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export Firestore
 export const db = getFirestore(app);
