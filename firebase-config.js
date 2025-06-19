12feb385a802b37c75a84ca5d53578a1eb469f76
holy-gee <takundangonisa7@gmail.com>
2025-06-19 21:09:55 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,9 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBPdGAZT_U8xNBsU-S4NnC7WUQI8zM1LWI",
   authDomain: "vidfind-77a6a.firebaseapp.com",
   projectId: "vidfind-77a6a",
   storageBucket: "vidfind-77a6a.appspot.com",
   messagingSenderId: "813301438270",
   appId: "1:813301438270:web:2ebe4dec657167c5403e6f",
-  measurementId: "G-N4NTHY2230",
+  measurementId: "G-N4NTHY2230"
 };
-
-// Initialize Firebase App
-const app = initializeApp(firebaseConfig);
-
-// Export Firestore DB instance
-export const db = getFirestore(app);
